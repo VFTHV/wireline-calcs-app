f5d972314331c2b519cc4ac04b56678ea98bd4c9
Vadim Fattakhov <vadfthv@gmail.com>
2024-03-31 23:11:44 -0500

    added scrollview to navpage

--- pages/NavPage.tsx
+++ pages/NavPage.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import React from 'react';
 import { NavItem } from '../components/NavItem';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -24,44 +24,46 @@ interface NavPageProps {
 
 export default function NavPage({ navigation }: NavPageProps) {
   return (
-    <View>
-      <NavItem navigation={navigation} to="Units">
-        Choose Units
-      </NavItem>
-      <NavItem navigation={navigation} to="WeakPoint">
-        Weak Point
-      </NavItem>
-      <NavItem navigation={navigation} to="WeightBar">
-        Weight Bar
-      </NavItem>
-      <NavItem navigation={navigation} to="Stretch">
-        Stretch
-      </NavItem>
-      <NavItem navigation={navigation} to="KeySeat">
-        KeySeat
-      </NavItem>
-      <NavItem navigation={navigation} to="TensionAtDepth">
-        Tension At Depth
-      </NavItem>
-      <NavItem navigation={navigation} to="CsgSpecs">
-        Casing Specs
-      </NavItem>
-      <NavItem navigation={navigation} to="TempCorrLength">
-        Temp. Corrected Length
-      </NavItem>
-      <NavItem navigation={navigation} to="CBL">
-        CBL Calcs
-      </NavItem>
-      <NavItem navigation={navigation} to="FluidVelocity">
-        Fluid Velocity
-      </NavItem>
-      <NavItem navigation={navigation} to="Feedback">
-        Feedback
-      </NavItem>
-      <NavItem navigation={navigation} to="Disclaimer">
-        Disclaimer
-      </NavItem>
-    </View>
+    <ScrollView>
+      <View>
+        <NavItem navigation={navigation} to="Units">
+          Choose Units
+        </NavItem>
+        <NavItem navigation={navigation} to="WeakPoint">
+          Weak Point
+        </NavItem>
+        <NavItem navigation={navigation} to="WeightBar">
+          Weight Bar
+        </NavItem>
+        <NavItem navigation={navigation} to="Stretch">
+          Stretch
+        </NavItem>
+        <NavItem navigation={navigation} to="KeySeat">
+          KeySeat
+        </NavItem>
+        <NavItem navigation={navigation} to="TensionAtDepth">
+          Tension At Depth
+        </NavItem>
+        <NavItem navigation={navigation} to="CsgSpecs">
+          Casing Specs
+        </NavItem>
+        <NavItem navigation={navigation} to="TempCorrLength">
+          Temp. Corrected Length
+        </NavItem>
+        <NavItem navigation={navigation} to="CBL">
+          CBL Calcs
+        </NavItem>
+        <NavItem navigation={navigation} to="FluidVelocity">
+          Fluid Velocity
+        </NavItem>
+        <NavItem navigation={navigation} to="Feedback">
+          Feedback
+        </NavItem>
+        <NavItem navigation={navigation} to="Disclaimer">
+          Disclaimer
+        </NavItem>
+      </View>
+    </ScrollView>
   );
 }
 
