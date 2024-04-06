d5eb7b2d0ae93d6a5974ffafbc7d4e72067f51d6
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-06 11:55:32 -0500

    CableManualEntrance and CurrentCableSpecs in Weakpoint.tsx

--- pages/WeakPoint.tsx
+++ pages/WeakPoint.tsx
@@ -2,22 +2,27 @@ import { StyleSheet, View } from 'react-native';
 import CableSelector from '../components/CableSelector';
 import { CurrentCableSpecs } from '../components/CurrentCableSpecs';
 import { CableManualEntrance } from '../components/CableManualEntrance';
+import { useSelector, useDispatch } from 'react-redux';
+import { StoreState } from '../store';
 
 export default function WeakPoint() {
+  const dispatch = useDispatch();
+  const { currentCable, toolWeight, depth, environment } = useSelector(
+    (state: StoreState) => state.weakPoint
+  );
+
   return (
     <View>
       <CableSelector />
-      {/* <CurrentCableSpecs
-        specs={[
-          'breakingStrength',
-          'outerArmorBS',
-          'weightInAir',
-          'maxTension',
-        ]}
-      /> */}
-      <CableManualEntrance
-        specs={['outerArmorBS', 'weightInAir', 'maxTension']}
-      />
+      {currentCable.type === 'MANUAL' ? (
+        <CableManualEntrance
+          specs={['outerArmorBS', 'weightInAir', 'maxTension']}
+        />
+      ) : (
+        <CurrentCableSpecs
+          specs={['outerArmorBS', 'weightInAir', 'maxTension']}
+        />
+      )}
     </View>
   );
 }
