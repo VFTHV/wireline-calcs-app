cc283ef4e8d0b7b04accc262673cbd6242ac137d
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-07 14:38:48 -0500

    PipeSelector.tsx

---
+++ components/PipeSelector.tsx
@@ -0,0 +1,72 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { ActionCreatorWithPayload } from '@reduxjs/toolkit';
+import { changeCasing, changeTubing } from '../store';
+import { PipeSpecs } from '../database/casingsTubings';
+import { Picker } from '@react-native-picker/picker';
+import { styleVariables as st } from '../styles/global';
+
+interface PipeSelectorProps {
+  pipeData: PipeSpecs[];
+  typeId: 'casing' | 'tubing';
+}
+
+export const PipeSelector = ({ pipeData, typeId }: PipeSelectorProps) => {
+  const ODs = [...new Set(pipeData.map((pipe) => pipe.nom))];
+  const [nom, setNom] = useState<string>('');
+
+  const pipeWeights = pipeData
+    .filter((pipe) => pipe.nom === nom)
+    .map((pipe) => pipe.weight);
+
+  const [weight, setWeight] = useState<number>(pipeWeights[0]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const selectedPipe = pipeData.find(
+      (csg) => csg.nom === nom && csg.weight === weight
+    );
+    dispatch(action(selectedPipe));
+  }, [nom, weight]);
+
+  const action: ActionCreatorWithPayload<PipeSpecs | undefined> =
+    typeId === 'casing' ? changeCasing : changeTubing;
+
+  const pipeWord = typeId.charAt(0).toUpperCase() + typeId.slice(1);
+
+  return (
+    <View style={inputGroup}>
+      <Text style={text}>{pipeWord} OD:</Text>
+      <View style={pickerView}>
+        <Picker
+          selectedValue={nom}
+          onValueChange={(value: string) => setNom(value)}
+        >
+          {ODs.map((od) => {
+            return <Picker.Item key={od} value={od} label={od} />;
+          })}
+        </Picker>
+      </View>
+    </View>
+  );
+};
+
+const { text, inputGroup, pickerView } = StyleSheet.create({
+  inputGroup: {
+    margin: st.spacingDefault,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  text: {
+    color: st.secondaryColor,
+    fontSize: st.fontSizePri,
+    fontFamily: 'Outfit-Light',
+  },
+  pickerView: {
+    height: 32,
+    borderRadius: 5,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+});
--- pages/CBL.tsx
+++ pages/CBL.tsx
@@ -1,10 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+// import {
+//   FluidSelector,
+//   InputData,
+//   NavHeader,
+//   PipeSelector,
+//   PipeSpecsDisplay,
+//   TableRow,
+// } from '../components';
 import { FluidSelector } from '../components/FluidSelector';
+// import { InputData } from '../components/InputData';
+// import
+import { casingData } from '../database/casingsTubings';
+import { useCblCalcs } from '../logics/useCblCalcs';
+import { useSelector } from 'react-redux';
+import { StoreState } from '../store';
+import { useConvertUnits } from '../logics/useConvertUnits';
+import { PipeSelector } from '../components/PipeSelector';
 
 export default function CBL() {
   return (
     <View>
+      <PipeSelector pipeData={casingData} typeId="casing" />
       <FluidSelector />
     </View>
   );
