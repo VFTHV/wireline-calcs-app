4b624d6192577885edf0553544601c7371a48017
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-07 14:29:12 -0500

    FluidSelector.tsx

---
+++ components/FluidSelector.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { fluidsData } from '../database/cbl';
+import { useDispatch, useSelector } from 'react-redux';
+import { StoreState, changeFluid } from '../store';
+import { Picker } from '@react-native-picker/picker';
+import { styleVariables as st } from '../styles/global';
+
+export const FluidSelector = () => {
+  const types = [...new Set(fluidsData.map((fluid) => fluid.type))];
+  const { fluid } = useSelector((store: StoreState) => store.cbl);
+  const dispatch = useDispatch();
+
+  return (
+    <View style={inputGroup}>
+      <Text style={text}>Fluid Type:</Text>
+      <View style={pickerView}>
+        <Picker
+          selectedValue={fluid.type}
+          onValueChange={(value: string) => dispatch(changeFluid(value))}
+        >
+          {types.map((type, i) => {
+            return <Picker.Item key={i} value={type} label={type} />;
+          })}
+        </Picker>
+      </View>
+    </View>
+  );
+};
+
+const { text, inputGroup, pickerView } = StyleSheet.create({
+  inputGroup: {
+    margin: st.spacingDefault,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  text: {
+    color: st.secondaryColor,
+    fontSize: st.fontSizePri,
+    fontFamily: 'Outfit-Light',
+  },
+  pickerView: {
+    height: 32,
+    borderRadius: 5,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+});
--- pages/CBL.tsx
+++ pages/CBL.tsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { FluidSelector } from '../components/FluidSelector';
 
 export default function CBL() {
   return (
     <View>
-      <Text>CBL</Text>
+      <FluidSelector />
     </View>
   );
 }
