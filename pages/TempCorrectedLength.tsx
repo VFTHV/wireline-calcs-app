110db8c0b9f37091f711161111988003011023c6
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-07 14:17:00 -0500

    TempCorrectedLength.tsx page

--- pages/TempCorrectedLength.tsx
+++ pages/TempCorrectedLength.tsx
@@ -1,10 +1,58 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import CableSelector from '../components/CableSelector';
+import { CurrentCableSpecs } from '../components/CurrentCableSpecs';
+import { InputData } from '../components/InputData';
+import TableRow from '../components/TableRow';
+import { useSelector } from 'react-redux';
+import { StoreState } from '../store';
+import { useTempLengthCalc } from '../logics/useTempLengthCalc';
 
 export default function TempCorrectedLength() {
+  const [temp, setTemp] = useState(0);
+  const [resistance, setResistance] = useState(0);
+  const { tempUnits, resistanceUnits } = useSelector(
+    (state: StoreState) => state.unitSystem
+  );
+
+  const { currentCable } = useSelector((state: StoreState) => state.weakPoint);
+  const { unitSystem } = useSelector((state: StoreState) => state);
+
+  const { length } = useTempLengthCalc(
+    resistance,
+    currentCable,
+    temp,
+    unitSystem
+  );
+
   return (
     <View>
-      <Text>TempCorrectedLength</Text>
+      <InputData
+        value={temp}
+        onChange={(value) => setTemp(+value)}
+        typeId="temperature"
+        unit={tempUnits}
+      >
+        Ambient Temperature
+      </InputData>
+      <InputData
+        value={resistance}
+        onChange={(value) => setResistance(+value)}
+        typeId="resistance"
+        unit={resistanceUnits}
+      >
+        Measured Cable Resistance
+      </InputData>
+
+      <CableSelector />
+      <CurrentCableSpecs specs={['conductorResistance']} />
+      <TableRow
+        data={length === Infinity ? 0 : length}
+        units={unitSystem.depthUnits}
+      >
+        Temperature Corrected Cable Length
+      </TableRow>
     </View>
   );
 }
