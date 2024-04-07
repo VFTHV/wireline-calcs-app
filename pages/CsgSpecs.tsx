5633bb53ef16ea789a7706e143e716bf065533c6
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-07 16:30:55 -0500

    CsgSpecs.tsx page

--- pages/CsgSpecs.tsx
+++ pages/CsgSpecs.tsx
@@ -1,12 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { casingData, tubingData } from '../database/casingsTubings';
+
+import { PipeSelector } from '../components/PipeSelector';
+import PipeSpecsDisplay from '../components/PipeSpecsDisplay';
 
 export default function CsgSpecs() {
   return (
     <View>
-      <Text>CsgSpecs</Text>
+      <PipeSelector pipeData={casingData} typeId="casing" />
+      <PipeSpecsDisplay
+        typeId="casing"
+        specs={['id', 'drift', 'capacity']}
+        pipeThck
+      />
+      <PipeSelector pipeData={tubingData} typeId="tubing" />
+      <PipeSpecsDisplay
+        typeId="tubing"
+        specs={['id', 'drift', 'capacity']}
+        pipeThck
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
