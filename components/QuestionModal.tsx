8f05c32e46c050d01ceab534252cee35c19b6ca0
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-09 21:56:23 -0500

    Styled QuestionModal.tsx

--- components/QuestionModal.tsx
+++ components/QuestionModal.tsx
@@ -1,28 +1,59 @@
-import { StyleSheet, Modal, TouchableOpacity, Text } from 'react-native';
+import {
+  StyleSheet,
+  Modal,
+  TouchableOpacity,
+  Text,
+  ScrollView,
+} from 'react-native';
 import React, { useState } from 'react';
 import { AntDesign } from '@expo/vector-icons';
+import { styleVariables as st } from '../styles/global';
 
 export default function QuestionModal() {
   const [isModalVisible, setIsModalVisible] = useState(false);
-  console.log(isModalVisible);
+
   return (
     <TouchableOpacity onPress={() => setIsModalVisible(!isModalVisible)}>
-      <AntDesign name="questioncircleo" size={24} color="black" />
+      <AntDesign name="questioncircleo" size={24} color="white" />
       <Modal
-        animationType="slide"
+        animationType="fade"
         transparent={true}
         visible={isModalVisible}
         onRequestClose={() => setIsModalVisible(false)}
       >
         <TouchableOpacity onPress={() => setIsModalVisible(!isModalVisible)}>
-          <Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quae
-            ea sunt dolor eius laborum ipsam consequatur blanditiis libero eos.
-          </Text>
+          <ScrollView style={container}>
+            <Text style={[standardText, header]}>Lorem, ipsum dolor.</Text>
+            <Text style={[standardText, content]}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+              voluptatum nihil corrupti odit reiciendis eum iusto ipsum
+              repudiandae quia vel odio facilis, sequi maiores eligendi
+              distinctio repellendus enim quidem dolor.
+            </Text>
+          </ScrollView>
         </TouchableOpacity>
       </Modal>
     </TouchableOpacity>
   );
 }
 
-const styles = StyleSheet.create({});
+const { container, standardText, header, content } = StyleSheet.create({
+  container: {
+    padding: st.spacingDefault,
+    backgroundColor: st.primaryColor,
+    height: '100%',
+  },
+  standardText: {
+    marginVertical: 5,
+    fontSize: st.fontSizePri,
+    color: st.secondaryColor,
+  },
+
+  header: {
+    textAlign: 'center',
+    fontFamily: 'Outfit-Bold',
+  },
+  content: {
+    fontFamily: 'Outfit-Light',
+  },
+});
