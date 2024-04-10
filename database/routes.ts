c71b1fb1624daa60974b2824ea94086ed916bf15
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-09 22:09:44 -0500

    extracted routing names into pathNames.ts

--- database/routes.ts
+++ database/routes.ts
@@ -1,14 +1,29 @@
-export const pathNames = {
-  units: '/units',
-  weakpoint: '/weakpoint',
-  weightbar: '/weightbar',
-  stretch: '/stretch',
-  keyseat: '/keyseat',
-  tensionAtDepth: '/tension-at-depth',
-  csgSpecs: '/csg-specs',
-  tempCorrLength: '/temp-corr-length',
-  cbl: '/cbl',
-  feedback: '/feedback',
-  disclaimer: '/disclaimer',
-  fluidVelocity: '/fluid-velocity',
+export type PathNamesType = {
+  units: 'Units';
+  weakpoint: 'WeakPoint';
+  weightbar: 'WeightBar';
+  stretch: 'Stretch';
+  keyseat: 'KeySeat';
+  tensionAtDepth: 'TensionAtDepth';
+  csgSpecs: 'CsgSpecs';
+  tempCorrLength: 'TempCorrLength';
+  cbl: 'CBL';
+  fluidVelocity: 'FluidVelocity';
+  feedback: 'Feedback';
+  disclaimer: 'Disclaimer';
+};
+
+export const pathNames: PathNamesType = {
+  units: 'Units',
+  weakpoint: 'WeakPoint',
+  weightbar: 'WeightBar',
+  stretch: 'Stretch',
+  keyseat: 'KeySeat',
+  tensionAtDepth: 'TensionAtDepth',
+  csgSpecs: 'CsgSpecs',
+  tempCorrLength: 'TempCorrLength',
+  cbl: 'CBL',
+  fluidVelocity: 'FluidVelocity',
+  feedback: 'Feedback',
+  disclaimer: 'Disclaimer',
 };
--- pages/NavPage.tsx
+++ pages/NavPage.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { NavItem } from '../components/NavItem';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { Feather } from '@expo/vector-icons';
+import { pathNames } from '../database/routes';
 
 export type RootStackParamList = {
   Units: undefined;
@@ -29,42 +30,42 @@ export default function NavPage({ navigation }: NavPageProps) {
       <View>
         <NavItem
           navigation={navigation}
-          to="Units"
+          to={pathNames.units}
           icon={<Feather name="settings" size={24} color="white" />}
         >
           Change Measurement Units
         </NavItem>
-        <NavItem navigation={navigation} to="WeakPoint">
+        <NavItem navigation={navigation} to={pathNames.weakpoint}>
           Weak Point Calculator
         </NavItem>
-        <NavItem navigation={navigation} to="WeightBar">
+        <NavItem navigation={navigation} to={pathNames.weightbar}>
           Sinker Bar Weight Calculator
         </NavItem>
-        <NavItem navigation={navigation} to="Stretch">
+        <NavItem navigation={navigation} to={pathNames.stretch}>
           Cable Stretch Calculator
         </NavItem>
-        <NavItem navigation={navigation} to="KeySeat">
+        <NavItem navigation={navigation} to={pathNames.keyseat}>
           Cable Stuck Depth {`(Keyseat)`}
         </NavItem>
-        <NavItem navigation={navigation} to="TensionAtDepth">
+        <NavItem navigation={navigation} to={pathNames.tensionAtDepth}>
           Max. Tension at Depth
         </NavItem>
-        <NavItem navigation={navigation} to="CsgSpecs">
+        <NavItem navigation={navigation} to={pathNames.csgSpecs}>
           Casing/Tubing Specs
         </NavItem>
-        <NavItem navigation={navigation} to="TempCorrLength">
+        <NavItem navigation={navigation} to={pathNames.tempCorrLength}>
           Temp. Corrected Length
         </NavItem>
-        <NavItem navigation={navigation} to="CBL">
+        <NavItem navigation={navigation} to={pathNames.cbl}>
           Cement Bond Log Calcs
         </NavItem>
-        <NavItem navigation={navigation} to="FluidVelocity">
+        <NavItem navigation={navigation} to={pathNames.fluidVelocity}>
           Fluid Velocity
         </NavItem>
-        {/* <NavItem navigation={navigation} to="Feedback">
+        {/* <NavItem navigation={navigation} to={pathNames.feedback}>
           Feedback
         </NavItem> */}
-        <NavItem navigation={navigation} to="Disclaimer">
+        <NavItem navigation={navigation} to={pathNames.disclaimer}>
           Disclaimer
         </NavItem>
       </View>
