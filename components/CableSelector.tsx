a871a48adc6d4bd27126df2c4f478fa84c888000
Vadim Fattakhov <vadfthv@gmail.com>
2024-04-12 22:54:55 -0500

    pickerview style

--- components/CableSelector.tsx
+++ components/CableSelector.tsx
@@ -14,6 +14,7 @@ export default function CableSelector() {
   // pickerview style
   // pickerview style
   // pickerview style
+  // pickerview style
   const handleCableChange = (itemValue: string) => {
     const selectedCable = cablesData.find((cable) => cable.type === itemValue);
     if (selectedCable) {
