8f7fe03c8797efeffbb1fccc7a1245185d6b70c9
Vadim Fattakhov <vadfthv@gmail.com>
2024-05-30 10:14:19 -0500

    SuccessResponseType

--- pages/Feedback.tsx
+++ pages/Feedback.tsx
@@ -34,6 +34,7 @@ type SuccessResponse = {
 // SuccessResponseType
 // SuccessResponseType
 // SuccessResponseType
+// SuccessResponseType
 
 export default function Feedback() {
   const [name, setName] = useState('');
