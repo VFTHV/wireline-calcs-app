cc5aa6ef475e6b1f2cad86103a22b6e0d475d17a
Vadim Fattakhov <vadfthv@gmail.com>
2024-11-16 17:09:50 -0600

    feat: validate email, integrate with formspree api

--- apis/axios.ts
+++ apis/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customFetch = axios.create({
-  baseURL: 'http://192.168.131.24:3000/',
+  baseURL: 'https://formspree.io/f/movqlalv',
   timeout: 1000 * 10,
   timeoutErrorMessage: 'Server may take 1 minute to start. Please try again.',
 });
